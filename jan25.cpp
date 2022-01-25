27d28d002968b29a8b50c96034d537234adb5a3c
Manish Upadhyay <upadhyaymanish302@gmail.com>
2022-01-25 15:10:21 +0530

    length of last word

---
+++ jan25.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        while(s[i]==' ') i--;
+        int count=0;
+        while(i>=0 and s[i]!=' ') count++,i--;
+        
+        return count;
+    }
+};
