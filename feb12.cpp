9f5fb756b8a0ee88b88f8c23530981617a3bc099
Manish Upadhyay <upadhyaymanish302@gmail.com>
2022-02-12 08:56:26 +0530

    valid anagram

---
+++ feb12.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size()!=t.size()) return 0;
+        
+        int arr[26]={0};
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        } 
+        for(int i=0;i<26;i++) if(arr[i]!=0) return 0;
+        return 1;
+    }
+};
